f19de87795284e9742db8dbb4213ca7c587505b6
DESKTOP-VG58M2H\samisofy <sami@sofy.ai>
2021-11-07 12:07:54 +0500

    Add testing SubZero File to TEMP project
    Signed-off-by: DESKTOP-VG58M2H\samisofy <sami@sofy.ai>

---
+++ JavaTutorial/Projects/RPG Game TEMP/Utilities/Globals.java
@@ -0,0 +1,5 @@
+package Utilities;
+
+public class Globals {
+    public static String saveDir = "C:\\Users\\92336\\Documents\\coding-tutorials\\JavaTutorial\\Projects\\RPG Game TEMP\\SaveFiles\\Characters";
+}
